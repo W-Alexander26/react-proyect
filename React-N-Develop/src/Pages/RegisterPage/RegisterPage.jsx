45b8a587eb83f280982389a869a9356f1878490a
Alexander <alexanderaguilar726@gmail.com>
2024-11-18 23:12:07 -0600

    Primera modularización del proyecto

---
+++ React-N-Develop/src/Pages/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./registerpage.css";
+import Register from "../../Components/Register/Register";
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <Register />
+    </div>
+  );
+};
+
+export default RegisterPage;
